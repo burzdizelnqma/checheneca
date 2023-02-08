5c3c783d8b50d14de21b273daf5fefc35452429c
nikolaihristov <124766753+nikolaihristov@users.noreply.github.com>
2023-02-08 11:02:52 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vuvedete chislo"); 
+            int n = int.Parse(Console.ReadLine());
+            int temp = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int a = 0; a < i; a++)
+                {
+                    Console.Write($"{temp++} ");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
